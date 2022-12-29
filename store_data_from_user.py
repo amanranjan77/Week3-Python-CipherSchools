a8780a14facc9472978f824ca74256454502be65
Aman Ranjan <115680272+amanranjan77@users.noreply.github.com>
2022-12-29 23:14:00 +0530

    Delete store_data_from_user.py

--- store_data_from_user.py
+++
@@ -1,8 +0,0 @@
-keys=input("enter keys: ").split(",")
-values=input("enter values: ").split(",")
-dect={}
-for k in range(0,len(keys)):
-    dect[keys[k]]=values[k]
-print(dect)
-for keys,values in dect.items():
-    print(keys,":",values)

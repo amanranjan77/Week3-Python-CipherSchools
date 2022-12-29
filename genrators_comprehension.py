82826cb65569112b6159928713e5bbcdbecefe92
Aman Ranjan <115680272+amanranjan77@users.noreply.github.com>
2022-12-29 23:09:26 +0530

    Delete genrators_comprehension.py

--- genrators_comprehension.py
+++
@@ -1,5 +0,0 @@
-gen1=(i**2 for i in range(1,80))
-# generating genrators is same like genration list just we use paranthesis instead of sq. brackets
-print(gen1)
-for i in gen1:
-    print(i,end="")
